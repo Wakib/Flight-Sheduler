b018161fba3734ac11d8a24f989f39876c4a39ff
Wakib <wakib55@gmail.com>
2019-11-21 15:10:01 +0600

    frontend service-list added

--- frontend/flight-scheduler/src/app/app.module.ts
+++ frontend/flight-scheduler/src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from "@angular/forms",
+import { FormsModule} from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClient } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
+import { FlightListComponent } from './flight-list/flight-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlightListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClient,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
